45cd4d68fb380053efe57976ea2c8e64116d7afe
jashuva <jthupakula@kpmg.com.au>
2023-03-23 21:10:42 +1100

    Image loading animation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,20 +23,26 @@ interface GamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     apiClient
       .get<GamesResponse>('/games', { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
+        setIsLoading(false);
         setError(err.message);
       });
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
