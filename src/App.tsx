eb2db41da8428dd08796b8b477db58149d828092
jashuva <jthupakula@kpmg.com.au>
2023-03-23 21:38:02 +1100

    create a generic data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-      }}
-    >
-      <GridItem area='nav'>
-        <NavBar />
-      </GridItem>
-      <Show above='lg'>
-        <GridItem area='aside'>aside</GridItem>
-      </Show>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`
+        }}
+      >
+        <GridItem area='nav'>
+          <NavBar />
+        </GridItem>
+        <Show above='lg'>
+          <GridItem area='aside'>
+            <GenreList />
+          </GridItem>
+        </Show>
 
-      <GridItem area='main'>
-        <GameGrid />
-      </GridItem>
-    </Grid>
+        <GridItem area='main'>
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-import { CanceledError } from 'axios';
-import apiClient from '../services/apiClient';
+import useData from './useData';
 
 export interface Platform {
   id: number;
@@ -15,34 +13,7 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
-interface GamesResponse {
-  count: number;
-  results: Game[];
-}
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-    apiClient
-      .get<GamesResponse>('/games', { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setIsLoading(false);
-        setError(err.message);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>('/games');
 
 export default useGames;
