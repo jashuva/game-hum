6f2a533a1dbbc90fdc497f8a36d536831a3f2a3d
jashuva <jthupakula@kpmg.com.au>
2023-03-23 20:54:14 +1100

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Text, Card, CardBody, Heading, Image, HStack } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import getCroppedImageUrl from '../services/imageUrl';
 import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
 
@@ -9,7 +10,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
