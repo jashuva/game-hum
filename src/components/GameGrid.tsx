5320bdf74dcebe0b57ad45bada576080dc21a05c
jashuva <jthupakula@kpmg.com.au>
2023-03-23 23:21:21 +1100

    Remove log

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,6 @@ interface Props {
 const GameGrid = ({ selectedGenre }: Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
-  console.log(selectedGenre?.name);
 
   return (
     <>
